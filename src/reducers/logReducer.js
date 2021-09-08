890d05585eb6531594c20f6b61327df91aa7ab06
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2021-09-08 02:13:14 +0100

    added my dispatch fun

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -9,6 +9,11 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case GET_LOGS:
+      return {
+        ...state,
+        logs: action.payload,
+      };
     case SET_LOADING:
       return {
         ...state,
