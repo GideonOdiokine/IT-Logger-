44c3aff956601106f8a90f498756d96c522a30c4
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2021-09-08 01:22:05 +0100

    added middleware to store

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from ".toolkit-SCML7RY9/dist/devtoolsExtension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+
+const initialState={
+    
+};
+
+const 
