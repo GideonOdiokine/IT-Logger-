faf12f0282f294332796a464106ac712233947e6
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2021-09-08 02:00:38 +0100

    logReducer added

---
+++ src/reducers/logReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+  logs: null,
+  loading: false,
+  current: null,
+  error: null,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
