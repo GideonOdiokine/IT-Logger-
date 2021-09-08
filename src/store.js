b7adf2cb70a20c85468c328f08c32e3592de9d83
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2021-09-08 01:23:50 +0100

    rootReducer and initialState added to store

--- src/store.js
+++ src/store.js
@@ -3,9 +3,11 @@ import { composeWithDevTools } from ".toolkit-SCML7RY9/dist/devtoolsExtension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
+const initialState = {};
 
-const initialState={
-    
-};
-
-const 
+const middleware = [thunk];
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
