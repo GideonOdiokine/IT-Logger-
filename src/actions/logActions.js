0b1ebcda9109b80144e74244269db4ef084af9cd
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2021-09-08 03:42:22 +0100

    delete log added

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,10 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG } from "./types";
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+} from "./types";
 
 export const getLogs = () => {
   return async (dispatch) => {
@@ -40,6 +46,33 @@ export const addLog = (log) => {
       dispatch({
         type: ADD_LOG,
         payload: data,
+      });
+    } catch (err) {
+      dispatch({
+        type: LOGS_ERROR,
+        payload: err,
+      });
+    }
+  };
+};
+
+// Delete log
+
+export const deleteLog = (id) => {
+  return async (dispatch) => {
+    try {
+      setLoading();
+
+      await fetch(`/logs/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      dispatch({
+        type: DELETE_LOG,
+        payload: id,
       });
     } catch (err) {
       dispatch({
--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Moment from "react-moment";
+import { connect } from "react-redux";
+import { deleteLog } from "../../actions/logActions";
+import M from "materialize-css/dist/js/materialize.min.js";
 
-const LogItem = ({ log }) => {
+const LogItem = ({ log, deleteLog }) => {
+  const deleteLogItem = () => {
+    deleteLog(log.id);
+    M.toast({ html: "Log deleted" });
+  };
   return (
     <li className="collection-item">
       <div>
@@ -20,11 +27,13 @@ const LogItem = ({ log }) => {
           <Moment format="MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
         </span>
         <a href="#!" className="secondary-content">
-          <i className="material-icons grey-text">delete </i>
+          <i className="material-icons grey-text" onClick={deleteLogItem}>
+            delete
+          </i>
         </a>
       </div>
     </li>
   );
 };
 
-export default LogItem;
+export default connect(null, { deleteLog })(LogItem);
