6654f867f6f53bd710cd5a5b4ad99df43fcda0ef
Gideon Odiokine <gidysmartodiokineh@gmail.com>
2021-09-12 17:25:12 +0100

    new tech was added to db.json

--- src/components/logs/EditModalLog.js
+++ src/components/logs/EditModalLog.js
@@ -1,12 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import M from "materialize-css/dist/js/materialize.min.js";
 import { connect } from "react-redux";
 import { updateLog } from "../../actions/logActions";
-import { getTechs } from "../../actions/techsActions";
 
-import { useEffect } from "react";
-
-const EditModalLog = ({ techs: { techs }, updateLog, current, getTechs }) => {
+const EditModalLog = ({ techs: { techs }, updateLog, current }) => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState("");
