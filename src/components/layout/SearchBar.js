acd3cc51b9de74d633d6f3bdf4871729770d8d64
GideonOdiokine <gidysmartodiokineh@gmail.com>
2022-02-15 01:01:19 +0100

    change app title

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 // import { searchLogs } from "../../actions/logActions";
 
 const SearchBar = () => {
